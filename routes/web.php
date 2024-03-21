8fc1872502beb42995114a205bed3dc20447f707
is61b <is61blp3i@gmail.com>
2024-03-21 11:10:44 +0700

    coba tes github

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tes-git', function () {
+    return "Halo, ini cuma coba pakai git";
+});
