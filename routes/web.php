953f38784fbfea55a420fa3431d19d63e002c86f
is61b <is61blp3i@gmail.com>
2024-04-25 11:53:36 +0700

    pemasangan laravel/u

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/tes-git', function () {
     return "Halo, ini cuma coba pakai git";
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
