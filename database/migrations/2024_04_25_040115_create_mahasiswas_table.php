4ea4018a1c91d4517469e4b90a4bd9e472e9bef2
is61b <is61blp3i@gmail.com>
2024-04-25 11:14:16 +0700

    membuat migration

--- database/migrations/2024_04_25_040115_create_mahasiswas_table.php
+++ database/migrations/2024_04_25_040115_create_mahasiswas_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('mahasiswas', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin', 'user'])->default('user');
-            $table->rememberToken();
+            $table->string('nim',50)->unique();
+            $table->string('nama',100);
+            $table->string('tempatLahir',100);
+            $table->date('tanggalLahir');
+            $table->string('jenisKelamin',20);
+            $table->string('agama',20);
+            $table->bigInteger('jurusans_id');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mahasiswas');
     }
 };
--- database/migrations/2024_04_25_041252_create_jurusans_table.php
+++ database/migrations/2024_04_25_041252_create_jurusans_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('jurusans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin', 'user'])->default('user');
-            $table->rememberToken();
+            $table->string('kode')->unique();
+            $table->string('jurusan');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('jurusans');
     }
 };
