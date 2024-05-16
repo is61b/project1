11d2ab29b450271875201aa4da87c1d6a1a16939
is61b <is61blp3i@gmail.com>
2024-05-16 11:40:50 +0700

    memnculkan modal ketika di klik tombol hapus

--- resources/views/jurusan/index.blade.php
+++ resources/views/jurusan/index.blade.php
@@ -45,7 +45,28 @@
                         <td>{{$item->jurusan}}</td>
                         <td>
                             <a href="/jurusan/edit/{{$item->id}}" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></a>
-                            <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+
+                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    ...
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
                     @empty
