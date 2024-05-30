5939720ee8cfc194d517254aadc926d48d0c9a6b
is61b <is61blp3i@gmail.com>
2024-05-30 10:55:38 +0700

    relasi model jurusan dan mahasiswa

--- app/Models/Jurusan.php
+++ app/Models/Jurusan.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Jurusan extends Model
 {
     use HasFactory;
+
+    public function mahasiswas(): BelongsTo
+    {
+        return $this->belongsTo(Mahasiswa::class);
+    }
 }
--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Mahasiswa extends Model
 {
     use HasFactory;
+
+    public function jurusans(): HasOne
+    {
+        return $this->hasOne(Jurusan::class, 'id','jurusans_id');
+    }
 }
