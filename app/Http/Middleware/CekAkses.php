d6ca0c588577096bcd60dc7ada5c2a3874733e82
is61b <is61blp3i@gmail.com>
2024-07-25 09:19:33 +0700

    pengaturan middleware hak akses

---
+++ app/Http/Middleware/CekAkses.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CekAkses
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$akses): Response
+    {
+        if(in_array(Auth::user()->hak_akses == $akses)){
+            return $next($request);
+        }
+        return redirect('/');
+
+    }
+}
