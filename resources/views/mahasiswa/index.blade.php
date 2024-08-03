257ffede0cabfe9105449db1f8c08a608bfee1d5
is61b <is61blp3i@gmail.com>
2024-08-03 11:11:18 +0700

    tampil foto mahasiswa yang telah diupload sebelumnya

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -74,6 +74,33 @@
                                 </div>
                                 </div>
                             </div>
+
+                            <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#foto{{$item->id}}">
+                                <i class="fa fa-eye"></i>
+                            </button>
+                            <!-- Modal -->
+                            <div class="modal fade" id="foto{{$item->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Foto {{$item->nama}}</h1>
+                                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img src="{{asset('foto/'.$item->foto)}}" width="100%">
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                    <form action="/mahasiswa/{{$item->id}}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">Hapus</button>
+                                    </form>
+
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
                     @empty
